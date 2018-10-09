1cec6cb19375c687173c40c7e5b4c173285c7dfe
anatali <anatali@nat480s>
2018-10-09 17:07:14 +0200

    bls18 docs

---
+++ it.unibo.bls18/src/it/unibo/bls/devices/raspberry/LedOnRaspberry.java
@@ -0,0 +1,15 @@
+package it.unibo.bls.devices.raspberry;
+
+
+/*
+ * Receives messages over a network connection working as a server
+ * 
+ */
+
+public class LedOnRaspberry  {
+
+ 
+}
+/*
+Runtime.getRuntime().exec("sudo bash led25GpioTurnOn.sh");
+*/
---
+++ it.unibo.bls18/src/it/unibo/bls/devices/raspberry/LedProxyForRaspberry.java
@@ -0,0 +1,17 @@
+package it.unibo.bls.devices.raspberry;
+
+import java.util.Observable;
+
+import it.unibo.bls.interfaces.IObserver;
+
+/*
+ * Sends messages over a network connection
+ */
+public class LedProxyForRaspberry implements IObserver{
+
+	@Override
+	public void update(Observable o, Object arg) {
+ 		
+	}
+
+}
