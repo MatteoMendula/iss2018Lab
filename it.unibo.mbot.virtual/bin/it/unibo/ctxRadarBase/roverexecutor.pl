a7baa240fd8b7c1d8fa94e8805599174bdf386fa
iugin <eugenio.pierfederici@studio.unibo.it>
2018-10-10 23:30:55 +0200

    Rimosso tutti i files nelle cartelle bin/

--- it.unibo.mbot.virtual/bin/it/unibo/ctxRadarBase/roverexecutor.pl
+++
@@ -1,5 +0,0 @@
-%====================================================================================
-% Context ctxRadarBase standalone= SYSTEM-configuration: file it.unibo.ctxRadarBase.roverExecutor.pl 
-%====================================================================================
-context(ctxradarbase, "localhost",  "TCP", "8033" ).  		 
-%%% -------------------------------------------
--- it.unibo.mbot/bin/it/unibo/ctxMbotExecutor/testmbotagent.pl
+++
@@ -1,5 +0,0 @@
-%====================================================================================
-% Context ctxMbotExecutor standalone= SYSTEM-configuration: file it.unibo.ctxMbotExecutor.testMbotAgent.pl 
-%====================================================================================
-context(ctxmbotexecutor, "localhost",  "TCP", "8029" ).  		 
-%%% -------------------------------------------
