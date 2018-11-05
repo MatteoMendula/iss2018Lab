619218b4543215bad1f4f9c7d18a03bc14f605c6
anatali <antonio.natali@unibo.it>
2018-11-05 14:46:48 +0100

    update robot

--- it.unibo.mbot2018/nodeCode/robot/serverRobotCmd.js
+++ it.unibo.mbot2018/nodeCode/robot/serverRobotCmd.js
@@ -3,17 +3,12 @@
 * serverRobotCmd.js
 * =====================================
 */
-const
- 	http    = require("http"),
-	parse   = require('url').parse,
-    join    = require('path').join,
-    srvUtil = require("./ServerUtils"),
-    fs      = require('fs'),
-    url     = require('url'),    
-    Readable = require('stream').Readable;
-
+const http    = require("http"),
+      join    = require('path').join ;
 
 const robotModel     = require('./models/robot');
+const robotControl   = require('./controllers/robotControl');
+const modelutils     = require('./utils/modelUtils');
 
 var serialPort ;
 var toVirtualRobot;
@@ -22,10 +17,10 @@ var myPort;
 const realRobot = false;
 
 if( realRobot ){    
-	serialPort = require('./serial');
+	serialPort = require('./utils/serial');
 	console.log("serialPort= " + serialPort.path  );
 }
-else   toVirtualRobot = require("./clientRobotVirtual");
+else   toVirtualRobot = require("./utils/clientRobotVirtual");
 
 /*
 * --------------------------------------------------------------
@@ -45,22 +40,36 @@ app.set("view engine", "ejs");			//npm install --save ejs
 */
     const server  = http.createServer( app );   
 	const io      = require('socket.io').listen(server); 	//npm install --save socket.io
+	
+	robotControl.setRealRobot( false );
  
+	app.use(express.static(__dirname + '/public'));
 /*
 * --------------------------------------------------------------
 * 3a) HANDLE A GET REQUEST
 * --------------------------------------------------------------
 */
 app.get('/', function(req, res) {
-	res.render("access");
+	var state  = robotModel.robot.state;
+	res.render('access', 
+		{'title': 'Robot Control', 'res': "Welcome", 'robotstate': state} 
+	); 
 });	
 
-app.get('/pi', function (req, res) {
- 	  res.end('This is the frontend-Pi!')
+app.get('/robotenv', function (req, res) {
+	var state     = robotModel.robot.state;
+	var envToShow = JSON.stringify( 
+			modelutils.modelToResources(robotModel.robotenv, false)
+			);
+	res.render('robotenv', 
+		{'title': 'Robot Environment', 'res': envToShow , 'model': robotModel.robotenv} 
+	); 
 });
-app.get('/robot/state', function (req, res) {
+app.get('/robotstate', function (req, res) {
 	var state  = robotModel.robot.state;
-	res.end('ROBOT STATE=' + state)
+	res.render('access', 
+			{'title': 'Robot Control', 'res': robotModel.robotenv , 'robotstate': state} 
+		); 
 });
 
 /*
@@ -69,97 +78,29 @@ app.get('/robot/state', function (req, res) {
 * --------------------------------------------------------------
 */
 
-app.post("/robot/actions/commands/w", function(req, res) { actuate("w", req, res); });	
-app.post("/robot/actions/commands/s", function(req, res) { actuate("s", req, res); });	
-app.post("/robot/actions/commands/h", function(req, res) { actuate("h", req, res); });	
-app.post("/robot/actions/commands/d", function(req, res) { actuate("d", req, res); });	
-app.post("/robot/actions/commands/a", function(req, res) { actuate("a", req, res); });		
+ 
+app.post("/robot/actions/commands/w", function(req, res, next) { 
+	robotControl.actuate("w", req, res ); next();});
+app.post("/robot/actions/commands/s", function(req, res, next) { 
+	robotControl.actuate("s", req, res ); next();});
+app.post("/robot/actions/commands/h", function(req, res, next) { 
+	robotControl.actuate("h", req, res ); next(); });
+app.post("/robot/actions/commands/d", function(req, res, next) { 
+	robotControl.actuate("d", req, res ); next(); });
+app.post("/robot/actions/commands/a", function(req, res, next) { 
+	robotControl.actuate("a", req, res ); next(); });
+		
+ 
 
-/*
-* --------------------------------------------------------------
-* ACTIONS
-* --------------------------------------------------------------
-*/
 
-function actuate( cmd, req, res ){
-	var newState     = "";
-	var cmdToVirtual = "";
-	console.log("actuate " + cmd  );
-	if( cmd === "w" ){ cmdToVirtual=`{ "type": "moveForward",  "arg": -1 }` ; 
-		newState="server moving forward"; }
-	else if( cmd === "s" ){ cmdToVirtual=`{ "type": "moveBackward",  "arg": -1 }` ; 
-		newState="server moving backward"; }
-	else if( cmd === "h" ){ cmdToVirtual=`{ "type": "alarm",  "arg": 1000 }` ; 
-			newState="server stopped"; }
-	else if( cmd === "a" ){ cmdToVirtual=`{ "type": "turnLeft",  "arg": 1000 }` ; 
-			newState="server  moving left"; }
-	else if( cmd === "d" ){ cmdToVirtual=`{ "type": "turnRight",  "arg": 1000 }` ; 
-		newState="server  moving right"; }
-	
-	if( realRobot){
-		actuateOnArduino( cmd, newState, req,res ) 
-	}else{
-		actuateOnVirtual( cmdToVirtual, newState, req,res )		
-	}
-	
-}
-function actuateOnVirtual(cmd, newState, req, res ){
-
-	console.log("actuateOnVirtual:" + cmd );
-  	toVirtualRobot.send( cmd );  	
- 	robotModel.robot.state = newState;
- 	
-		setTimeout( function(){
-			//var now = new Date().toUTCString();
-			io.sockets.send( newState ); //+ " time=" + now
-		}, 200 ) ;
-
-  	res.render("access");
-}
-
-function actuateOnArduino(cmd, newState, req, res ){
-	console.log("actuateOnArduino: " + cmd + " " + serialPort.path);
-	serialPort.write(cmd);	
-}
-
-function delegate( hlcmd, newState, req, res ){
- 	robotModel.robot.state = newState;
-	emitRobotCmd(hlcmd);
-    res.render("access");	
-}
-var emitRobotCmd = function( cmd ){ //called by delegate;
- 	var eventstr = "msg(usercmd,event,js,none,usercmd(robotgui( " +cmd + ")),1)"
-  		console.log("emits> "+ eventstr);
-// 		mqttUtils.publish( eventstr );	//topic  = "unibo/qasys";
-}
-
-/*
-* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-* UTILITIES
-* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-*/
-var requestUtil = function(req,res,cb){
-	var urlParts    = url.parse(req.url, true),
-		urlParams   = urlParts.query, 
-		urlPathname = urlParts.pathname,
-		body = '',
-		reqInfo = {};
-
-	req.on('data', function (data) {
-	 body += data; 
-	});
-	req.on('end', function () {
-		reqInfo.method      = req.method;		//'GET'
-		reqInfo.urlPathname = urlPathname;  	//'/api/user'
-		reqInfo.urlParams   = urlParams;  		//{}
-		reqInfo.body        = body;  
-		reqInfo.query       = urlParts.query;	//{}
-		reqInfo.urlParts    = urlParts;
-		//console.log( reqInfo    );
-		cb(reqInfo,res);
-	 });	
-	}
-
+app.use( function(req,res){ 
+	console.log("last " + req.myresult );
+	//console.log(  req  );
+	res.render('access', 
+		{'title': 'Robot Control', 'res': req.myresult, 'robotstate':robotModel.robot.state} 
+	); 
+} );
+ 
 /*
 * %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 * EXCEPTIONS
